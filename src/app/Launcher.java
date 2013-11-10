da7d21614b6d29a0a767ecee59b53a85fb7ac090
admin <admin@192.168.0.101>
2013-11-10 14:07:34 +0100

    Extracted interface ViewFactory

--- src/app/Launcher.java
+++ src/app/Launcher.java
@@ -5,7 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ToolBar;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -24,12 +23,12 @@ public class Launcher extends Application {
             toolbar, main
         ).build();
         
-        Button btn1 = new Button("Calendar");
-        btn1.setOnAction(new SetMainContent(main, "Calendar"));
-        Button btn2 = new Button("Workers");
-        btn2.setOnAction(new SetMainContent(main, "Worker"));
+        Button calendarBtn = new Button("Calendar");
+        calendarBtn.setOnAction(new SetMainContent(main, new Calendar()));
+        Button workersBtn = new Button("Workers");
+        workersBtn.setOnAction(new SetMainContent(main, new Workers()));
         
-        toolbar.getItems().addAll(btn1, btn2);
+        toolbar.getItems().addAll(calendarBtn, workersBtn);
         
         Scene scene = new Scene(root, 800, 600);
         primaryStage.setTitle("APP.NAME");
@@ -40,16 +39,16 @@ public class Launcher extends Application {
     private static class SetMainContent implements EventHandler<ActionEvent> {
         
         private final StackPane main;
-        private final String content;
+        private final ViewFactory factory;
         
-        private SetMainContent(StackPane main, String content) {
+        private SetMainContent(StackPane main, ViewFactory factory) {
             this.main = main;
-            this.content = content;
+            this.factory = factory;
         }
 
         @Override
         public void handle(ActionEvent t) {
-            main.getChildren().setAll(new Label(content));
+            main.getChildren().setAll(factory.build());
         }
     }
 
