4e2e201992ac96d0c4e691cf82cbede66c09403e
Raffaele Sgarro <raffaeleSgarro@gmail.com>
2013-11-10 16:50:34 +0100

    Renamed fixture.sql to seeds.sql

--- resources/sql/fixture.sql
+++
@@ -1,9 +0,0 @@
-insert into worker (id, first_name, last_name) values
-  (1, 'Joe J.', 'Foo')
-, (2, 'Allison B.', 'Bar')
-;
-
-insert into worked_hour(worker_id, day, hours, hourly_wage) values
-  (1, '2013-11-10', 7, 8.5)
-, (1, '2013-11-10', 6, 8.5)
-;
