ebe35d3cb080ec52d799c82a26084da61a25bc57
Raffaele Sgarro <raffaelesgarro@gmail.com>
2014-01-18 18:33:49 +0100

    Watch for checbox selection

--- src/app/Payments.java
+++ src/app/Payments.java
@@ -1,8 +1,12 @@
 package app;
 
 import beans.Payment;
+import beans.WorkHour;
 import dao.PaymentDao;
 import dao.WorkHourDao;
+import java.math.BigDecimal;
+import javafx.collections.ListChangeListener;
+import javafx.collections.SetChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
@@ -10,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBoxBuilder;
 import javafx.scene.layout.VBox;
 import shared.DatePicker;
 
@@ -21,23 +26,56 @@ public class Payments {
     private final PaymentDao paymentDao;
     private final WorkHourDao workHourDao;
     
+    // payment subform
+    private final TextField amount = new TextField();
+    private final TextField hourlyRate = new TextField();
+    private final Label selectedTotalHours = new Label();
+    private final Button computeAmountBtn = new Button("Calcola");
+    
+    // select hours
+    private final SelectHours selectHours = new SelectHours();
+
     private int workerId;
     
     public Payments(PaymentDao dao, WorkHourDao workHourDao) {
         this.paymentDao = dao;
         this.workHourDao = workHourDao;
+        
         newPaymentBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 showNewPaymentForm(workerId);
             }
         });
+        
+       selectHours.getSelectedHours().addListener(new SetChangeListener<WorkHour>(){
+            @Override
+            public void onChanged(SetChangeListener.Change<? extends WorkHour> change) {
+                BigDecimal total = BigDecimal.ZERO;
+                for (WorkHour wh : change.getSet()) {
+                    if (wh.hours != null)
+                        total = total.add(wh.hours);
+                }
+                selectedTotalHours.setText(String.format("Totale selezionato %.1f ore", total));
+            }
+        });
     }
     
     public void showNewPaymentForm(int workerId) {
         this.workerId = workerId;
-        SelectHours selectHours = new SelectHours();
-        root.getChildren().setAll(new DatePicker(), new TextField("totale"), new Label("Dettaglio ore"), selectHours.getWidget());
+        root.getChildren().setAll(
+                  HBoxBuilder.create().children(
+                          new Label("Data pagamento")
+                          , new DatePicker()).build()
+                , HBoxBuilder.create().children(
+                        new Label("Totale")
+                        , amount
+                        , selectedTotalHours
+                        , hourlyRate
+                        , computeAmountBtn).build()
+                , new Label("Dettaglio ore")
+                , selectHours.getWidget());
+        
         selectHours.setHours(workHourDao.findUnpaid(workerId));
     }
     
--- src/app/SelectHours.java
+++ src/app/SelectHours.java
@@ -2,6 +2,12 @@ package app;
 
 import beans.WorkHour;
 import java.util.List;
+import java.util.Set;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -18,13 +24,14 @@ public class SelectHours {
     private final Button cancelBtn = new Button("Annulla");
     private final Label total = new Label("Totale");
     private final ListView<WorkHour> hours = new ListView<>();
+    private final ObservableSet<WorkHour> selectedHoursProperty = FXCollections.observableSet();
     
     public SelectHours() {
         hours.setCellFactory(new Callback<ListView<WorkHour>, ListCell<WorkHour>>() {
 
             @Override
             public ListCell<WorkHour> call(ListView<WorkHour> p) {
-                return new SelectHourCell();
+                return new SelectHourCell(selectedHoursProperty);
             }
         });
     }
@@ -34,11 +41,28 @@ public class SelectHours {
     }
     
     private static class SelectHourCell extends ListCell<WorkHour> {
+        
+        private final Set<WorkHour> ctx;
+        
+        public SelectHourCell(Set<WorkHour> ctx) {
+            this.ctx = ctx;
+        }
+        
         @Override
-        public void updateItem(WorkHour item, boolean empty) {
+        public void updateItem(final WorkHour item, boolean empty) {
             super.updateItem(item, empty);
             if (!empty) {
                 CheckBox checkbox = new CheckBox();
+                checkbox.selectedProperty().addListener(new ChangeListener<Boolean>(){
+                    @Override
+                    public void changed(ObservableValue<? extends Boolean> ov, Boolean t, Boolean t1) {
+                        if (t1) {
+                            ctx.add(item);
+                        } else {
+                            ctx.remove(item);
+                        }
+                    }
+                });
                 String text = item.date.toString() + " (" + item.hours + ")";
                 setGraphic(HBoxBuilder.create().children(checkbox, new Label(text)).build());
             } else {
@@ -49,6 +73,10 @@ public class SelectHours {
     
     public Node getWidget() {
         return VBoxBuilder.create().children(total, hours, HBoxBuilder.create().children(saveBtn, cancelBtn).build()).build();
+    }
+    
+    public ObservableSet<WorkHour> getSelectedHours() {
+        return selectedHoursProperty;
     }
     
 }
